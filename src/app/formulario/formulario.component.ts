7256158fa4cea6290d32ba3d06c35c4075cf4841
PauloRenato98 <paulo__159@hotmail.com>
2020-02-16 15:32:12 -0300

    html e ts estão funcionando

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-formulario',
   templateUrl: './formulario.component.html',
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
+
+  email = '';
+  name = '';
+  endereco = '';  
+  cidade = '';
+
+
+
 
   constructor() { }
 
